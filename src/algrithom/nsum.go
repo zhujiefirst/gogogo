f9ef5e0c4597a4a6b2a3fa72c42f5694761180ec
zhujiefirst <zhujiefirst@gmail.com>
2014-11-13 23:48:55 +0800

    add ThreeSum(Fast) and its test, with some bugs

--- src/algrithom/nsum.go
+++ src/algrithom/nsum.go
@@ -1,7 +1,7 @@
 package algrithom
 
 import (
-	// "log"
+	"log"
 	"sort"
 )
 
@@ -18,6 +18,23 @@ func TwoSum(na []int32) (cnt int32) {
 	return
 }
 
+// 找出所有和为0的三元整数对的数量
+func ThreeSum(na []int32) (cnt int32) {
+	l := len(na)
+	// log.Println(na)
+	for i := 0; i < l; i++ {
+		for j := i + 1; j < l; j++ {
+			for k := j + 1; k < l; k++ {
+				// log.Printf("%v+%v+%v=%v", na[i], na[j], na[k], na[i]+na[j]+na[k])
+				if na[i]+na[j]+na[k] == 0 {
+					cnt++
+				}
+			}
+		}
+	}
+	return
+}
+
 type nums []int32
 
 func (a nums) Len() int           { return len(a) }
@@ -27,12 +44,13 @@ func (a nums) Less(i, j int) bool { return a[i] < a[j] }
 // 找出所有和为0的整数对的数量
 // 先sort，再用bin-search方法寻找和为0的整数对
 func TwoSumFast(na []int32) (cnt int32) {
-	// log.Println("before sort:", na)
+	log.Println("before sort:", na)
 	sort.Sort(nums(na))
-	// log.Println("before after:", na)
+	log.Println("after sort:", na)
 
 	for i, v := range na {
 		idx := sort.Search(len(na), func(i int) bool { return na[i] >= -v })
+		log.Printf("na[%v]=%v, idx=%v", i, v, idx)
 		if idx != len(na) && i < idx && na[idx] == -v {
 			// log.Printf("lv=%v[%v], rv=%v[%v]", v, i, na[idx], idx)
 			cnt++
@@ -43,6 +61,38 @@ func TwoSumFast(na []int32) (cnt int32) {
 					continue
 				}
 				break
+			}
+		}
+	}
+
+	return
+}
+
+func ThreeSumFast(na []int32) (cnt int32) {
+	// log.Println("before sort:", na)
+	sort.Sort(nums(na))
+	// log.Println("after sort:", na)
+
+	for i, vi := range na {
+		for j, vj := range na[i:] {
+			v := vi + vj
+			idx := sort.Search(len(na), func(i int) bool { return na[i] >= -v })
+			if idx != len(na) && func(a, b int) int {
+				if a > b {
+					return a
+				}
+				return b
+			}(i, j) < idx && na[idx] == -v {
+				// log.Printf("lv=%v[%v], rv=%v[%v]", v, i, na[idx], idx)
+				cnt++
+				for {
+					idx++
+					if idx < len(na) && na[idx] == -v {
+						cnt++
+						continue
+					}
+					break
+				}
 			}
 		}
 	}
--- src/algrithom/nsum_test.go
+++ src/algrithom/nsum_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	N = 100000
+	N = 5
 )
 
 func init() {
@@ -26,4 +26,17 @@ func TestTwoSum(*testing.T) {
 
 	tb = time.Now()
 	log.Printf("在数量为%v的随机整数数组中，和为0的整数对数量为%v, expend=%v", len(na), TwoSumFast(na), time.Now().Sub(tb))
+}
+
+func TestThreeSum(*testing.T) {
+	var na []int32
+	for i := 0; i < N; i++ {
+		na = append(na, rand.Int31n(N)-N/2)
+	}
+
+	tb := time.Now()
+	log.Printf("在数量为%v的随机整数数组中，和为0的三元整数对数量为%v, expend=%v", len(na), ThreeSum(na), time.Now().Sub(tb))
+
+	tb = time.Now()
+	log.Printf("在数量为%v的随机整数数组中，和为0的三元整数对数量为%v, expend=%v", len(na), ThreeSumFast(na), time.Now().Sub(tb))
 }
