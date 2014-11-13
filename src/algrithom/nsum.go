39cc7e85768d611b7c3fdf168c0917c26082b134
zhujiefirst <zhujiefirst@gmail.com>
2014-11-13 23:17:51 +0800

    add TwoSumFast & its test

--- src/algrithom/nsum.go
+++ src/algrithom/nsum.go
@@ -1,14 +1,51 @@
 package algrithom
 
+import (
+	// "log"
+	"sort"
+)
+
 // 找出所有和为0的整数对的数量
 func TwoSum(na []int32) (cnt int32) {
 	l := len(na)
 	for i := 0; i < l; i++ {
-		for j := i; j < l; j++ {
+		for j := i + 1; j < l; j++ {
 			if na[i]+na[j] == 0 {
 				cnt++
 			}
 		}
 	}
+	return
+}
+
+type nums []int32
+
+func (a nums) Len() int           { return len(a) }
+func (a nums) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a nums) Less(i, j int) bool { return a[i] < a[j] }
+
+// 找出所有和为0的整数对的数量
+// 先sort，再用bin-search方法寻找和为0的整数对
+func TwoSumFast(na []int32) (cnt int32) {
+	// log.Println("before sort:", na)
+	sort.Sort(nums(na))
+	// log.Println("before after:", na)
+
+	for i, v := range na {
+		idx := sort.Search(len(na), func(i int) bool { return na[i] >= -v })
+		if idx != len(na) && i < idx && na[idx] == -v {
+			// log.Printf("lv=%v[%v], rv=%v[%v]", v, i, na[idx], idx)
+			cnt++
+			for {
+				idx++
+				if idx < len(na) && na[idx] == -v {
+					cnt++
+					continue
+				}
+				break
+			}
+		}
+	}
+
 	return
 }
--- src/algrithom/nsum_test.go
+++ src/algrithom/nsum_test.go
@@ -4,10 +4,11 @@ import (
 	"log"
 	"math/rand"
 	"testing"
+	"time"
 )
 
 const (
-	N = 10000
+	N = 100000
 )
 
 func init() {
@@ -19,5 +20,10 @@ func TestTwoSum(*testing.T) {
 	for i := 0; i < N; i++ {
 		na = append(na, rand.Int31n(N)-N/2)
 	}
-	log.Printf("在数量为%v的随机整数数组中，和为0的整数对数量为%v\n", len(na), TwoSum(na))
+
+	tb := time.Now()
+	log.Printf("在数量为%v的随机整数数组中，和为0的整数对数量为%v, expend=%v", len(na), TwoSum(na), time.Now().Sub(tb))
+
+	tb = time.Now()
+	log.Printf("在数量为%v的随机整数数组中，和为0的整数对数量为%v, expend=%v", len(na), TwoSumFast(na), time.Now().Sub(tb))
 }
