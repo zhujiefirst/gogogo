168266cfa17f5221191b575216f99dd517fa91f7
zhujiefirst <zhujiefirst@gmail.com>
2014-12-14 16:45:17 +0800

    ent

--- src/algrithom/sort/sort_test.go
+++ src/algrithom/sort/sort_test.go
@@ -2,13 +2,14 @@ package sort
 
 import (
 	"fmt"
-	"math/rand"
+	// "math/rand"
 	"testing"
 )
 
 func compareTo(lhs Comparable, rhs Comparable) int {
 	a := lhs.(int)
 	b := rhs.(int)
+	// fmt.Printf("a=%v\tb=%v\n", a, b)
 	if a < b {
 		return -1
 	} else if a > b {
@@ -21,18 +22,38 @@ func compareTo(lhs Comparable, rhs Comparable) int {
 func genRandom(n int) []Comparable {
 	var a []Comparable
 	for i := 0; i < n; i++ {
-		e := rand.Int() % (2 * n)
+		// e := rand.Int() % (2 * n)
+		// e := i
+		e := n - i
 		a = append(a, e)
 	}
 	return a
 }
 
 func TestSort(*testing.T) {
-	c := genRandom(10)
+	n := 10
+	var c = make([]Comparable, n)
+	var cc []Comparable
+	var selectSort *SelectSort
+	var insertionSort *InsertionSort
 
-	sort := new(SelectSort)
-	sort.Show(c)
-	sort.Sort(c, compareTo)
-	sort.Show(c)
-	fmt.Printf("compare count=%v, exch count=%v\n", sort.compareCount, sort.exchCount)
+	cc = genRandom(n)
+
+	copy(c[0:], cc[0:])
+	selectSort = new(SelectSort)
+	fmt.Println("before select sort...")
+	selectSort.Show(c)
+	selectSort.Sort(c, compareTo)
+	fmt.Println("after select sort...")
+	selectSort.Show(c)
+	fmt.Printf("for select sort compare count=%v, exch count=%v\n", selectSort.compareCount, selectSort.exchCount)
+
+	copy(c, cc)
+	insertionSort = new(InsertionSort)
+	fmt.Println("before insertion sort...")
+	insertionSort.Show(c)
+	insertionSort.Sort(c, compareTo)
+	fmt.Println("after insertion sort...")
+	insertionSort.Show(c)
+	fmt.Printf("for insertion sort, compare count=%v, exch count=%v\n", insertionSort.compareCount, insertionSort.exchCount)
 }
